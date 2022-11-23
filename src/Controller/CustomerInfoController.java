cdb29cc96a5045754d50888570f38caf58d30a22
LachelleAllen <lachelleallen@icloud.com>
2022-11-23 06:53:32 -0700

    Customers Model constructor, getter and setter, CustomerInfo.fxml table columns updated, CustomerInfoController tableview updated.

--- src/Controller/CustomerInfoController.java
+++ src/Controller/CustomerInfoController.java
@@ -1,22 +1,56 @@
 package Controller;
 
+import Model.Customers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class CustomerInfoController {
 
         @FXML
-        private TableView<?> customerTableView;
+        private TableView<Customers> customerTableView;
 
-        @FXML
+    @FXML
+    private TableColumn<Customers, String> customerTableAddress;
+
+    @FXML
+    private TableColumn<Customers, LocalDateTime> customerTableCreateDate;
+
+    @FXML
+    private TableColumn<Customers, String> customerTableCreatedBy;
+
+    @FXML
+    private TableColumn<Customers, Integer> customerTableDivisionId;
+
+    @FXML
+    private TableColumn<Customers, Integer> customerTableId;
+
+    @FXML
+    private TableColumn<Customers, LocalDateTime> customerTableLastUpdate;
+
+    @FXML
+    private TableColumn<Customers, String> customerTableLastUpdatedBy;
+
+    @FXML
+    private TableColumn<Customers, String> customerTableName;
+
+    @FXML
+    private TableColumn<Customers, String> customerTablePhone;
+
+    @FXML
+    private TableColumn<Customers, String> customerTablePostalCode;
+
+
+    @FXML
         void addCustomerButton(ActionEvent event) throws IOException {
             Parent parent = FXMLLoader.load(getClass().getResource("/View/AddCustomer.fxml"));
             Scene scene = new Scene(parent);
--- src/Model/Customers.java
+++ src/Model/Customers.java
@@ -1,5 +1,7 @@
 package Model;
 
+import java.time.LocalDateTime;
+
 public class Customers {
 
     int customerID;
@@ -7,5 +9,102 @@ public class Customers {
     String address;
     String postalCode;
     String phone;
+    LocalDateTime createDate;
+    String createdBy;
+    LocalDateTime updatedDate;
+    String updatedBy;
     int divsionID;
+
+    public Customers(int customerID, String customerName, String address, String postalCode, String phone, LocalDateTime createDate, String createdBy, LocalDateTime updatedDate, String updatedBy, int divsionID) {
+        this.customerID = customerID;
+        this.customerName = customerName;
+        this.address = address;
+        this.postalCode = postalCode;
+        this.phone = phone;
+        this.createDate = createDate;
+        this.createdBy = createdBy;
+        this.updatedDate = updatedDate;
+        this.updatedBy = updatedBy;
+        this.divsionID = divsionID;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(LocalDateTime updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public int getDivsionID() {
+        return divsionID;
+    }
+
+    public void setDivsionID(int divsionID) {
+        this.divsionID = divsionID;
+    }
 }

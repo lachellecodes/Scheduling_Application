08f398b281744c38d734fc33cc4e6f5bb75b9917
LachelleAllen <lachelleallen@icloud.com>
2022-12-21 21:23:09 -0700

    Login controller updated to check for overlapping appointments

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -35,144 +35,167 @@ import java.util.TimeZone;
 public class LoginController implements Initializable {
 
 
-        @FXML
-        private Label passwordLabel;
+    @FXML
+    private Label passwordLabel;
 
-        @FXML
-        private Label userIdLabel;
+    @FXML
+    private Label userIdLabel;
 
-        @FXML
-        private Label zoneIdLabel;
+    @FXML
+    private Label zoneIdLabel;
 
-        @FXML
-        private Label loginTitleLabel;
+    @FXML
+    private Label loginTitleLabel;
 
-        @FXML
-        private PasswordField passwordText;
+    @FXML
+    private PasswordField passwordText;
 
-        @FXML
-        private TextField userIDtext;
+    @FXML
+    private TextField userIDtext;
 
-        @FXML
-        private TextField zoneText;
+    @FXML
+    private TextField zoneText;
 
-        @FXML
-        private Button loginButton;
+    @FXML
+    private Button loginButton;
 
-        private ObservableList<Appointments> allAppointments= AppointmentDAO.getAllAppointments();
+    private ObservableList<Appointments> allAppointments = AppointmentDAO.getAllAppointments();
 
-        /** Gets the default time zone for the user's locale.*/
-        private  TimeZone userTimeZone = TimeZone.getDefault();
+    /**
+     * Gets the default time zone for the user's locale.
+     */
+    private TimeZone userTimeZone = TimeZone.getDefault();
 
-        Users currentUser;
+    Users currentUser;
 
     public LoginController() throws SQLException {
     }
 
-    /** Loads the next screen (Dashboard) after log in if user is successfully verified. */
-        public void loadDashboard (ActionEvent event) throws IOException {
-            Parent parent = FXMLLoader.load(getClass().getResource("/View/Dashboard.fxml"));
-            Scene scene = new Scene(parent);
-            Stage stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
-            stage.setScene(scene);
-            stage.show();
-        }
-        /** A method to verify if user enters the correct information. Gets text entered into the username and password field on
-         * the log in form. Uses a SQL prepared statement to match these strings against database. If there is a match, returns
-         * a true Boolean variable. */
-        public Boolean validateUser () throws SQLException {
-            
-            Boolean userVerified = false;
+    /**
+     * Loads the next screen (Dashboard) after log in if user is successfully verified.
+     */
+    public void loadDashboard(ActionEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("/View/Dashboard.fxml"));
+        Scene scene = new Scene(parent);
+        Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 
-            String user = userIDtext.getText();
-            String password = passwordText.getText();
+    /**
+     * A method to verify if user enters the correct information. Gets text entered into the username and password field on
+     * the log in form. Uses a SQL prepared statement to match these strings against database. If there is a match, returns
+     * a true Boolean variable.
+     */
+    public Boolean validateUser() throws SQLException {
+
+        Boolean userVerified = false;
+
+        String user = userIDtext.getText();
+        String password = passwordText.getText();
 
 
-            String verifyLogin = "SELECT * FROM users WHERE User_Name = '" + user + "' AND Password  = '"+password + "'";
+        String verifyLogin = "SELECT * FROM users WHERE User_Name = '" + user + "' AND Password  = '" + password + "'";
 
-            try{
+        try {
 
-             PreparedStatement ps= DBConnection.connection.prepareStatement(verifyLogin);
-             ResultSet rs = ps.executeQuery();
+            PreparedStatement ps = DBConnection.connection.prepareStatement(verifyLogin);
+            ResultSet rs = ps.executeQuery();
 
-             while(rs.next()){
+            while (rs.next()) {
 
-                 String userName = rs.getString("User_Name");
-                 String userPassword = rs.getString("Password");
+                String userName = rs.getString("User_Name");
+                String userPassword = rs.getString("Password");
 
-                 userVerified = true;}
-
+                userVerified = true;
             }
-            catch (Exception e){
-                e.printStackTrace();
-            }
-            return userVerified;
 
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        /** Method that handles the login in button action. Displays an alert message if the user ID and password fields are blank.
-         * Calls the validateUser method to validate credentials. If user is validated calls the loadDashboard method to go
-         * to the next screen. If user is not validated, displays an error message to enter a valid user ID and password.*/
-        @FXML
-        void loginButton(ActionEvent event) throws SQLException, IOException {
+        return userVerified;
 
-            if (userIDtext.getText().isBlank() == true || passwordText.getText().isBlank() == true) {
+    }
 
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle(rb.getString("alertTitle"));
-                alert.setHeaderText(rb.getString("errorMessage1"));
-                alert.show();
-            } else {
-                if (validateUser()) {
-                    loadDashboard(event);
+    /**
+     * Method that handles the login in button action. Displays an alert message if the user ID and password fields are blank.
+     * Calls the validateUser method to validate credentials. If user is validated calls the loadDashboard method to go
+     * to the next screen. If user is not validated, displays an error message to enter a valid user ID and password.
+     */
+    @FXML
+    void loginButton(ActionEvent event) throws SQLException, IOException {
 
-                    for(Appointments appointments: allAppointments){
+        if (userIDtext.getText().isBlank() == true || passwordText.getText().isBlank() == true) {
 
-                        String userName= userIDtext.getText();
-                        currentUser = UserDaoImpl.getUserById(userName);
-                        int currentUserID = currentUser.getUserID();
-                        LocalDateTime start = appointments.getStartDateTime();
-                        LocalDateTime now = LocalDateTime.now();
-                       // ZoneId zoneId= ZoneId.systemDefault();
-                       // ZonedDateTime zonedDateTime= ZonedDateTime.of(start, zoneId);
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle(rb.getString("alertTitle"));
+            alert.setHeaderText(rb.getString("errorMessage1"));
+            alert.show();
+        }
+        else {
+            if (validateUser()) {
+                loadDashboard(event);
 
-                        //ZoneId  utcZone = ZoneId.of("UTC");
-                        //ZonedDateTime  utcZoneDT = ZonedDateTime.ofInstant(zonedDateTime.toInstant(), utcZone);
-                        boolean appointmentOverlap = false;
+                for (Appointments appointments : allAppointments) {
+
+                    String userName = userIDtext.getText();
+                    currentUser = UserDaoImpl.getUserById(userName);
+                    int currentUserID = currentUser.getUserID();
+                    LocalDateTime start = appointments.getStartDateTime();
+                    LocalDateTime now = LocalDateTime.now();
+                    // ZoneId zoneId= ZoneId.systemDefault();
+                    // ZonedDateTime zonedDateTime= ZonedDateTime.of(start, zoneId);
+
+                    //ZoneId  utcZone = ZoneId.of("UTC");
+                    //ZonedDateTime  utcZoneDT = ZonedDateTime.ofInstant(zonedDateTime.toInstant(), utcZone);
 
 
-                        if (currentUserID == appointments.getApptUserID() && start.isAfter(now) && start.isBefore(now.plusMinutes(15))){
 
-                            appointmentOverlap= true;}
-
-                        if (appointmentOverlap){
+                    if (currentUserID == appointments.getApptUserID() && start.isAfter(now) && start.isBefore(now.plusMinutes(15))) {
 
 
-                            Alert alert = new Alert(Alert.AlertType.WARNING);
-                            alert.setTitle("Warning");
-                            alert.setHeaderText(" You have an appointment starting within the next 15 minutes.");
-                            alert.show();
+                    Alert alert = new Alert(Alert.AlertType.WARNING);
+                    alert.setTitle("Warning");
+                    alert.setHeaderText(" You have an appointment starting within the next 15 minutes.");
+                    alert.show();
 
-                        }
+                    break;
+                    //TODO  how do i stop this from cycling through the rest of the appointments
 
-                        else {
-                            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                            alert.setHeaderText("You have no upcoming appointments within the next 15 minutes. ");
-                            alert.show();
+                }
 
-                        }
+                    else {
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setHeaderText("You have no upcoming appointments within the next 15 minutes. ");
+                        alert.show();
 
                     }
-
                 }
-                    else {
-                    Alert alert = new Alert(Alert.AlertType.ERROR);
-                    alert.setTitle(rb.getString("alertTitle"));
-                    alert.setHeaderText(rb.getString("errorMessage2"));
-                    alert.show();
-                }
-
             }
+
+
+             else {
+
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle(rb.getString("alertTitle"));
+            alert.setHeaderText(rb.getString("errorMessage2"));
+            alert.show();
+
+
         }
+    }
+
+
+        }
+
+
+
+
+
+
+
+
+
     /** Gets the users current locale. */
     Locale currentLocale = Locale.getDefault();
 
